f7b08dac8e1b552c1cfae8f3651418da260894cc
Shilpa Soni <shilpassoni@gmail.com>
2020-04-14 19:11:30 +0530

    Updates

---
+++ precache-manifest.40a5213a1f533a31fd164051c575a6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c495a42dd992fdac6b75ce18b2877e98",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "c2b78839a4dff0ff99f9",
+    "url": "/random-quote-machine/static/css/main.a56ea50b.chunk.css"
+  },
+  {
+    "revision": "1b4d01a783a42f4bf0df",
+    "url": "/random-quote-machine/static/js/2.36ff2c07.chunk.js"
+  },
+  {
+    "revision": "65dfdf0d9c5c13c2912f91ed4a1f6a8f",
+    "url": "/random-quote-machine/static/js/2.36ff2c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b78839a4dff0ff99f9",
+    "url": "/random-quote-machine/static/js/main.8e7ca58b.chunk.js"
+  },
+  {
+    "revision": "95cd68c4eee61cb3b3c3",
+    "url": "/random-quote-machine/static/js/runtime-main.8156d79e.js"
+  }
+]);
